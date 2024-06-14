7c53c41e42884cd28ff4dfd2f4cb3d89d7152608
ntp-bi <bintp22122002@gmail.com>
2024-06-14 19:03:56 +0700

    push make layout

---
+++ src/pages/myGigs/MyGigs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./my-gigs.scss"
+
+const MyGigs = () => {
+  return (
+    <div>
+      MyGigs
+    </div>
+  )
+}
+
+export default MyGigs
---
+++ src/pages/orders/Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./order.scss"
+
+const Orders = () => {
+  return (
+    <div>
+      Orders
+    </div>
+  )
+}
+
+export default Orders
